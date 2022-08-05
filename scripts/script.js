6d53baa7451c1a61fbd9d80b7e8339b616102169
Tennessine <aliefesobeli007@gmail.com>
2022-08-05 18:50:32 +0300

    Add win/lose conditions

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,41 @@
 function getComputerChoice() {
-    const choices = ["Rock", "Paper", "Scissors"];
+    const choices = ["rock", "paper", "scissors"];
     let selectionIndex = Math.floor(Math.random()*3)
     return choices[selectionIndex];
 }
+
+function toTitleCase(string) {
+    const firstLetter = string[0].toUpperCase();
+    const restOfString = string.slice(1).toLowerCase();
+    return firstLetter + restOfString
+}
+
+function playRound(playerSelection, computerSelection) {
+
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+
+    if (playerSelection === "rock" && computerSelection === "scissors"
+     || playerSelection === "scissors" && computerSelection === "paper"
+     || playerSelection === "paper" && computerSelection === "rock") {
+        return `You win! ${toTitleCase(playerSelection)} beats ${computerSelection.toLowerCase()}!`
+    } else if (playerSelection === "scissors" && computerSelection === "rock"
+            || playerSelection === "paper" && computerSelection === "scissors"
+            || playerSelection === "rock" && computerSelection === "paper") {
+        return `You lose! ${toTitleCase(computerSelection)} beats ${playerSelection.toLowerCase()}!`
+    } else if (playerSelection === computerSelection) {
+        return "It's a draw! Nobody wins!";
+    } else {
+        return;
+    }
+}
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        let playerChoice = prompt("What's your move?", "Rock");
+        let result = playRound(playerChoice, getComputerChoice());
+        alert(result);
+    }
+}
+
+game();
